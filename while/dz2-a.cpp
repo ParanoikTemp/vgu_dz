0a00715d6d56e12acae67e61518ea33f3b83926a
ParanoikTemp <64136866+ParanoikTemp@users.noreply.github.com>
2022-09-18 16:49:35 +0300

    Create dz2-a.cpp

---
+++ while/dz2-a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float i = 0, sum = 0;
+    while (i <= 50) {
+        ++i;
+        sum += 1.0 / (i * i * i);
+    }
+    cout << sum;
+    return 0;
+}
