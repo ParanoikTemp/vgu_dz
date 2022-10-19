2a086cd6a8ccbc94e88ea07dc3d967331e0f3947
ParanoikTemp <64136866+ParanoikTemp@users.noreply.github.com>
2022-10-19 20:46:52 +0300

    Update 60_b.cpp

--- main-for/60_b.cpp
+++ main-for/60_b.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main() {
     float x1 = 1, x2 = 0.3, x3, sum=1.3;
-    for (int i = 3; i <= 20; ++i) {
+    for (float i = 3; i <= 20; ++i) {
         x3 = (i + 1) * x1;
-        sum += x3
+        sum += x3;
         x1 = x2; x2 = x3;
     }
     cout << sum;
