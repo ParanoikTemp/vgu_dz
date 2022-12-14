31830ccad964d4ba0fe26237ca658f096bf88041
ParanoikTemp <64136866+ParanoikTemp@users.noreply.github.com>
2022-12-14 09:26:54 +0300

    Update 4.cpp

--- kr3_v1/4.cpp
+++ kr3_v1/4.cpp
@@ -2,24 +2,30 @@
 
 using namespace std;
 
+int ccount(char x, char *str, int n) {
+    int c = 0;
+    for (int j = 0; j < n; ++j) {
+        if (str[j] == x) ++c;
+    }
+    return c;
+}
+
 int main()
 {
-    int n, count;
+    int n;
     cin >> n;
     char **chars = new char * [n], x;
-    cin >> x;
+    cin.ignore();
+    cin.get(x);
     for (int i = 0; i < n; ++i) {
         chars[i] = new char [n];
         for (int j = 0; j < n; ++j) chars[i][j] = (char) rand() % 256;
     }
 
     for (int i = 0; i < n; ++i) {
-        count = 0;
-        for (int j = 0; j < n; ++j) {
-            if (chars[i][j] == x) ++count;
-            cout << chars[i][j] << " ";
-        }
-        cout << "| " << count << endl;
+        for (int j = 0; j < n; ++j) cout << chars[i][j] << " ";
+        cout << "| " << ccount(x, chars[i], n) << endl;
     }
     return 0;
 }
+
