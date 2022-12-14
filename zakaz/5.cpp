9f0f5892020f3140e7d7e2ccd512ec2fbac49e42
ParanoikTemp <64136866+ParanoikTemp@users.noreply.github.com>
2022-12-14 21:12:19 +0300

    Create 5.cpp

---
+++ zakaz/5.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+    double x, E, element, summa1 = 1, summa2 = 0, function;
+    int N, count = 0;
+    cin >> x >> N >> E;
+    E *= 11;
+    for (int i = 1; i < N; ++i) {
+        int fact = 1;
+        for (int j = 1; j <= i; ++j, fact *= i);
+        summa1 += pow(x, i) * pow(-1, i) / (float) fact;
+    }
+    element = 1;
+    while (abs(element) > E) {
+        summa2 += element;
+        ++count;
+        int fact = 1;
+        for (int j = 1; j <= count; ++j, fact *= count);
+        element = pow(x, count) * pow(-1, count) / (float) fact;
+    }
+    function = pow(M_E, -1 * x);
+    cout << "e^-x = " << function << endl;
+    cout << "Summa 1: " << summa1 << " |e^-x - summa 1| = " << function - summa1 << endl;
+    cout << "Summa 2 (" << count + 1 << " elements): " << summa2 << " |e^-x - summa 2| = " << function - summa2 << endl;
+}
