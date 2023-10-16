04e8f9b6bb7fa1d85d9fbd49bf537d8df3d2fcef
Parax <64136866+ParanoikTemp@users.noreply.github.com>
2023-10-16 12:44:51 +0300

    Create classes.cpp

---
+++ 2sem/labs/laba4 Human->Student/classes.cpp
@@ -0,0 +1,41 @@
+#include "classes.h"
+
+Man::Man(string name, int age, bool sex, float weight) {
+    this->name = new string(name);
+    this->age = new int(age);
+    this->sex = new bool(sex);
+    this->weight = new float(weight);
+}
+
+void Man::setName(string name) {
+    *(this->name) = name;
+}
+
+void Man::setAge(int age) {
+    *(this->age) = age;
+}
+
+void Man::setSex(bool sex) {
+    *(this->sex) = sex;
+}
+
+void Man::setWeight(float weight) {
+    *(this->weight) = weight;
+}
+
+Student::Student(string name, int age, bool sex, float weight, unsigned int year): Man(name, age, sex, weight) {
+    yearOfStudy = new unsigned int(year);
+}
+
+void Student::setYearOfStudy(unsigned int year) {
+    *yearOfStudy = year;
+}
+
+Student& Student::operator++() {
+    ++(*yearOfStudy);
+    return *this;
+}
+
+void Student::print() {
+    cout << *name << ' ' << *age << ' ' << *sex << ' ' << *weight << ' ' << *yearOfStudy << endl;
+}
