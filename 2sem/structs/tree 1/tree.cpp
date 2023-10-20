e02d8ba8a51c9ae2793c921ba0dc16fffc3b1e8f
Parax <64136866+ParanoikTemp@users.noreply.github.com>
2023-10-20 23:01:13 +0300

    Update tree.cpp

--- 2sem/structs/tree 1/tree.cpp
+++ 2sem/structs/tree 1/tree.cpp
@@ -1,22 +1,32 @@
 #include "tree.h"
 
-void Tree::add(const string word, TreeNode* node = nullptr)
+void Tree::add(const string word, TreeNode* node)
 {
 	if (node == nullptr) node = head;
-	switch (node->word.compare(word))
-	{
-	case 1:
+	if (node == nullptr) {
+	    head = node = new TreeNode{word};
+	    return;
+	}
+	int res = node->word.compare(word);
+	if (res > 0) {
 		if (node->right != nullptr) add(word, node->right);
 		else node->right = new TreeNode{ word };
 		return;
-	case -1:
+	}
+	if (res < 0) {
 		if (node->left != nullptr) add(word, node->left);
 		else node->left = new TreeNode{ word };
 		return;
-	case 0:
-		node->count += 1;
-		return;
 	}
+	node->count += 1;
+	return;
+}
+
+void Tree::printCount(TreeNode* node) {
+    if (node == nullptr) node = head;
+    std::cout << node->word << ": " << node->count << std::endl;
+    if (node->right != nullptr) printCount(node->right);
+    if (node->left != nullptr) printCount(node->left);
 }
 
 // Лаба в разработке
