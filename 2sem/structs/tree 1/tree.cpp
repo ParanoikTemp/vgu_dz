067c9f20c0b92422a1732a05056c6bb7623ebd9b
Parax <64136866+ParanoikTemp@users.noreply.github.com>
2023-10-24 12:58:50 +0300

    Update tree.cpp

--- 2sem/structs/tree 1/tree.cpp
+++ 2sem/structs/tree 1/tree.cpp
@@ -39,4 +39,43 @@ void Tree::printTree(TreeNode* node, int level, int side) {
     if (node->left != nullptr) printTree(node->left, level + 1, -1);
 }
 
+TreeNode* Tree::findPrev(const string word, TreeNode* node) {
+    if (node == nullptr) node = head;
+    if (node->right != nullptr && node->right.word == word || node->left != nullptr && node->left.word == word) return node;
+    int res = node->word.compare(word);
+    if (res > 0 && node->right != nullptr) return findPrev(word, node->right);
+    if (res < 0 && node->left != nullptr) return findPrev(word, node->left);
+    return nullptr;
+}
+
+TreeNode* Tree::find(const string word) {
+    TreeNode* prev = findPrev(word);
+    if (prev == nullptr) {
+        if (head->word == word) return prev;
+        return nullptr;
+    }
+    if (prev->right != nullptr && prev->right.word == word) return prev->right;
+    if (prev->left != nullptr && prev->left.word == word) return prev->left;
+    return nullptr;
+}
+
+TreeNode* Tree::find(const string word, TreeNode* node) {
+    if (node->right != nullptr && node->right.word == word) return node->right;
+    return node->left;
+}
+
+int Tree::removeElement(const string word) {
+    TreeNode* prev = findPrev(word);
+    if (prev == nullptr) {
+        TreeNode* node = find(word);
+        if (node == nullptr) return 0;
+        // удаление корневого элемента
+    } else {
+        TreeNode* node = find(word, prev);
+        int count = node->count;
+        if (node->right == nullptr) {prev = node->left; return count;}
+        if (node->left == nullptr) {prev = node->right; return count;}
+        // доделать удаление с двумя ветвями
+    }
+}
 // Лаба в разработке
