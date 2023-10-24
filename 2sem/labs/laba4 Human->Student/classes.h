1cccc89702ceec80947ee234fb6b39c1b15c8956
Parax <64136866+ParanoikTemp@users.noreply.github.com>
2023-10-24 11:01:26 +0300

    Update classes.h

--- 2sem/labs/laba4 Human->Student/classes.h
+++ 2sem/labs/laba4 Human->Student/classes.h
@@ -11,17 +11,19 @@ protected:
     float* weight;
 public:
     Man(string name, int age, bool sex, float weight);
+    virtual ~Man();
     void setName(string name);
     void setAge(int age);
     void setSex(bool sex);
     void setWeight(float weight);
 };
 
-class Student: public Man {
+class Student : public Man {
 private:
     unsigned int* yearOfStudy;
 public:
     Student(string name, int age, bool sex, float weight, unsigned int year);
+    virtual ~Student();
     void setYearOfStudy(unsigned int year);
     Student& operator++();
     void print();
