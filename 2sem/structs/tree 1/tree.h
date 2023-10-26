46c25365d07870a954e5f6e73668e4cfe7d11a52
Parax <64136866+ParanoikTemp@users.noreply.github.com>
2023-10-26 12:17:43 +0300

    Update tree.h

--- 2sem/structs/tree 1/tree.h
+++ 2sem/structs/tree 1/tree.h
@@ -15,11 +15,11 @@ class Tree
 {
 private:
     TreeNode* head = nullptr;
-    TreeNode* findMin(string notWord, TreeNode* node = nullptr);
+    void remove(TreeNode* node, TreeNode* rmnode);
 public:
-    void add(const string word, TreeNode* node = nullptr);
+    void add(const string word, int count = 1, TreeNode* node = nullptr);
     void printCount(TreeNode* node = nullptr);
     void printTree(TreeNode* node = nullptr, int level = 0, int side = 0);
-    int removeElement(const string word, TreeNode* node = nullptr);
+    int removeElement(const string word, TreeNode* node = nullptr, int level = 0);
     void setCount(const string word, const int count);
 };
