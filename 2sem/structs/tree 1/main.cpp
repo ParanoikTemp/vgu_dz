b65fbee55e705eb741fe3ba2e9aac7609065b98f
Parax <64136866+ParanoikTemp@users.noreply.github.com>
2023-10-26 12:18:03 +0300

    Update main.cpp

--- 2sem/structs/tree 1/main.cpp
+++ 2sem/structs/tree 1/main.cpp
@@ -2,16 +2,17 @@
 #include<string>
 #include"tree.h"
 #include<fstream>
-#include <windows.h>
+//#include <windows.h>
 
 using namespace std;
 
 int main()
 {
-    SetConsoleOutputCP(CP_UTF8);
-    setlocale(LC_ALL, "RU");
+    //SetConsoleOutputCP(CP_UTF8);
+    //setlocale(LC_ALL, "RU");
     Tree tree, tree2;
-    ifstream file("C:\\cpp_files\\2sem\\structs\\Tree1\\words.txt");
+    //ifstream file("C:\\cpp_files\\2sem\\structs\\Tree1\\words.txt");
+    ifstream file("words.txt");
     if (file.is_open()) {
         string word;
         for (int i = 0; i < 250; ++i) {
@@ -20,13 +21,13 @@ int main()
         }
     }
     file.close();
-    ifstream file2("C:\\cpp_files\\2sem\\structs\\Tree1\\words2.txt");
+    //ifstream file2("C:\\cpp_files\\2sem\\structs\\Tree1\\words2.txt");
+    ifstream file2("words2.txt");
     if (file2.is_open()) {
         string word;
         for (int i = 0; i < 4; ++i) {
             file2 >> word;
-            tree2.add(word);
-            tree.removeElement(word);
+            tree2.add(word, tree.removeElement(word));
         }
     }
     tree.printTree();
