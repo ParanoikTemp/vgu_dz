a8ebf6223e2b7c700ce3832095f88d36106a7f4a
Parax <64136866+ParanoikTemp@users.noreply.github.com>
2023-10-26 12:17:19 +0300

    Update tree.cpp

--- 2sem/structs/tree 1/tree.cpp
+++ 2sem/structs/tree 1/tree.cpp
@@ -1,29 +1,21 @@
 #include "tree.h"
 
-TreeNode* Tree::findMin(string notWord, TreeNode* node)
-{
-    if (node == nullptr) node = head;  // Изначально идем с головы дерева
-    if (node == nullptr) return nullptr;  // Если головы нет то пи***ц
-    if (node->left != nullptr)
-    return nullptr;
-}
-
-void Tree::add(const string word, TreeNode* node)
+void Tree::add(const string word, int count, TreeNode* node)
 {
     if (node == nullptr) node = head;  // На начало рекурсии, мы ставим элемент на головной
     if (node == nullptr) {  // Если дерево пустое, то добавляем первый элемент
-        head = node = new TreeNode{ word };
+        head = node = new TreeNode{ word, count };
         return;
     }
     int res = node->word.compare(word);  // производим сравнение
     if (res < 0) {  // Если наше слово БОЛЬШЕ чем в узле, то идем вправо
-        if (node->right != nullptr) add(word, node->right);  // Если справа есть ветвь, то рекурсивно повторяем действие для правой ветви
-        else node->right = new TreeNode{ word };  // Иначе создаем новый лист справа
+        if (node->right != nullptr) add(word, count, node->right);  // Если справа есть ветвь, то рекурсивно повторяем действие для правой ветви
+        else node->right = new TreeNode{ word, count };  // Иначе создаем новый лист справа
         return;
     }
     if (res > 0) { // Если наше слово МЕНЬШЕ чем в узле, то идем влево
-        if (node->left != nullptr) add(word, node->left);  // Если слева есть ветвь, то рекурсивно повторяем действие для левой ветви
-        else node->left = new TreeNode{ word };  // Иначе создаем новый лист слева
+        if (node->left != nullptr) add(word, count, node->left);  // Если слева есть ветвь, то рекурсивно повторяем действие для левой ветви
+        else node->left = new TreeNode{ word, count };  // Иначе создаем новый лист слева
         return;
     }
     node->count += 1;  // Если слова совпадают, то увеличиваем кол-во слов на 1
@@ -49,44 +41,36 @@ void Tree::printTree(TreeNode* node, int level, int side) {
     if (node->left != nullptr) printTree(node->left, level + 1, -1);  // После вывода самого элемента, мы можем уже вывести и левые ветви
 }
 
-int Tree::removeElement(const string word, TreeNode* node) {
-    if (node == nullptr) node = head;  // Если пусто, то голова
-    if (node == nullptr) return 0;  // Если головы нет, то ниче нет
-    int count;
-    if (node->word == word) {  // Если совпадает, то это головной элемент. Если удаляем головной элемент, то очищаем значение, а головную ссылку ведем в пустоту
-        count = node->count;
-        delete node;
-        head = nullptr;
-    }
-    else {
-        TreeNode* rmnode = nullptr; // Удаляемая нода
-        int res = node->word.compare(word);  // производим сравнение
-        if (res < 0) {  // Если наше слово БОЛЬШЕ чем в узле, то идем вправо
-            if (node->right == nullptr) return 0;  // Если справа числа нет, то капут. Нет такого элемента!
-            if (node->right->word == word) rmnode = node->right;  // Если следующий элемент совпадает, то делаем на него ссылку
-            else return removeElement(word, node->right);  // Иначе рекурсивно идем вправо
-            if (rmnode->left == nullptr) {  // Если нет левой ветви, то просто заменяем элемент на тот, что справа.
-                count = rmnode->count;
-                node->right = rmnode->right;
-                delete rmnode;
-                return count;
-            }
-            else if (rmnode->right == nullptr) {  // Если нет правой ветви, то просто заменяем элемент на тот, что слева.
-                count = rmnode->count;
-                node->right = rmnode->left;
-                delete rmnode;
-                return count;
-            }
-            // Дописать удаление :(
-        }
-        if (res > 0) { // Если наше слово МЕНЬШЕ чем в узле, то идем влево
-            if (node->left == nullptr) return 0;  // Если слева числа нет, то капут. Нет такого элемента!
-            if (node->left->word == word) rmnode = node->left;  // Если следующий элемент совпадает, то делаем на него ссылку
-            else return removeElement(word, node->left);  // Иначе рекурсивно идем влево
-        }
-    }
+int Tree::removeElement(const string word, TreeNode* node, int level) {
+    if (head == nullptr) return 0;
+    if (level == 0) node = head;
+    if (node == nullptr) return 0;
     
-    return count;  // Возвращаем количество удаленных слов
+    int res = node->word.compare(word);  // производим сравнение
+    if (res < 0) return removeElement(word, node->right, level + 1);
+    else if (res > 0) return removeElement(word, node->left, level + 1);
+    else {
+        int count = node->count;
+        if (node->right == nullptr || node->left == nullptr) {
+            TreeNode* rmnode = (node->right == nullptr ? node->left : node->right);
+            *(node) = *(rmnode);
+            delete rmnode;
+        } else remove(node->left, node);
+        return count;
+    }
+}
+
+void Tree::remove(TreeNode* node, TreeNode* rmnode) {
+    if (node->right != nullptr) remove(node->right, rmnode);
+    else {
+        rmnode->word = node->word;
+        rmnode->count = node->count;
+        if (node->left != nullptr) {
+            TreeNode* rm2node = node->left;
+            *node = *(node->left);
+            delete rm2node;
+        }
+    }
 }
 
 void Tree::setCount(const string word, const int count) {
