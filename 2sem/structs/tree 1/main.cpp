9329a8e5a3b0c87d4b1045b2652e5a5e35871846
Parax <64136866+ParanoikTemp@users.noreply.github.com>
2023-10-31 23:51:58 +0300

    Update main.cpp

--- 2sem/structs/tree 1/main.cpp
+++ 2sem/structs/tree 1/main.cpp
@@ -1,37 +1,32 @@
-#include<iostream>
-#include<string>
-#include"tree.h"
-#include<fstream>
-//#include <windows.h>
+#include <string>
+#include "StringTree.h"
+#include <fstream>
+#include <windows.h>
+
 
 using namespace std;
 
-int main()
-{
-    //SetConsoleOutputCP(CP_UTF8);
-    //setlocale(LC_ALL, "RU");
-    Tree tree, tree2;
-    //ifstream file("C:\\cpp_files\\2sem\\structs\\Tree1\\words.txt");
-    ifstream file("words.txt");
-    if (file.is_open()) {
-        string word;
-        for (int i = 0; i < 250; ++i) {
-            file >> word;
-            tree.add(word);
-        }
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    setlocale(LC_ALL, "RU");
+    StringTree tree;
+    ifstream file("C:\\cpp_files\\2sem\\structs\\tree1.2\\words.txt");
+    string word;
+    for (int i = 0; i < 250; ++i) {
+        file >> word;
+        tree.insert(word);
     }
     file.close();
-    //ifstream file2("C:\\cpp_files\\2sem\\structs\\Tree1\\words2.txt");
-    ifstream file2("words2.txt");
-    if (file2.is_open()) {
-        string word;
-        for (int i = 0; i < 4; ++i) {
-            file2 >> word;
-            tree2.add(word, tree.removeElement(word));
-        }
-    }
     tree.printTree();
-    cout << "\n\n\n\n";
+    cout << "\n\n\n";
+    StringTree tree2;
+    ifstream file2("C:\\cpp_files\\2sem\\structs\\tree1.2\\words2.txt");
+    for (int i = 0; i < 4; ++i) {
+        file2 >> word;
+        tree2.insert(word, tree.remove(word));
+    }
     tree2.printTree();
+    cout << "\n\n\n";
+    tree.printTree();
     return 0;
 }
