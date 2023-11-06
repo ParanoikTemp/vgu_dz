7649ea37e5544f281e3a7e27eb2d8741635defdd
Parax <64136866+ParanoikTemp@users.noreply.github.com>
2023-11-07 01:46:10 +0300

    Update student.h

--- 2sem/labs/laba4 Human->Student/student.h
+++ 2sem/labs/laba4 Human->Student/student.h
@@ -4,11 +4,42 @@ class Student: public Man {
 private:
     unsigned int* yearOfStudy;
 public:
+    /**
+     * Конструтор студента
+     * @param name - Имя студента
+     * @param age - Возраст студента
+     * @param sex - Пол студента
+     * @param weight - Вес студента
+     * @param year - год обучения студента
+     */
     Student(string name, int age, bool sex, float weight, unsigned int year);
+    /**
+     * Конструтор копирования
+     * @param other - Объект класса Student
+     */
     Student(const Student &other);
-    virtual ~Student();
+    /**
+     * Деструктор класса Student
+     */
+    ~Student() override;
+    /**
+     * Сеттер года обучения студента
+     * @param year - год обучения студента
+     */
     void setYearOfStudy(unsigned int year);
+    /**
+     * Оператор инкремента
+     * @return возвращает текущий объект
+     */
     Student& operator++();
+    /**
+     * Оператор сравнения
+     * @param other - объект класса Student
+     * @return возвращает текущий объект
+     */
     Student& operator= (const Student &other);
+    /**
+     * Выводит в консоль объект
+     */
     void print();
 };
