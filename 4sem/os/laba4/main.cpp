583e34146f31ec6c07a484a2fec17a64adf3d84e
Parax <64136866+ParanoikTemp@users.noreply.github.com>
2024-06-07 11:57:04 +0300

    Create main.cpp

---
+++ 4sem/os/laba4/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "calculations.h"
+
+int main() {
+int m, n;
+std::cout << "Введите m и n: ";
+std::cin >> m >> n;
+
+int k = findMaxWithSameDigitSum(m, n);
+std::cout << "Наибольшее число с суммой цифр как у " << m << " между " << m << " и " << n << ": " << k << std::endl;
+
+return 0;
+}
